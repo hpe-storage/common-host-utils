f419a013b0b6a43aa204537639d9a615db07dc4e
Mike Berhan <berhan@hpe.com>
2019-08-27 12:30:04 -0700

    Add version information to chapi2client.exe * Problem:   * For Windows, we want automatic versioning for chapi2client.exe * Implementation:   * Updated chapi2client.go to include required go generate options   * Added versioninfo.json with executable version template   * Added chapi2client.exe.manifest to require executable to run with Admin rights * Testing:   * Built successfully in Linux VM; verified executable is versioned * Reviewers:   * @shivamerla, @suneeth51 * Bug: N/A

--- cmd/chapi2client/chapi2client.go
+++ cmd/chapi2client/chapi2client.go
@@ -1,3 +1,6 @@
+//go:generate updatewinversioninfo
+//go:generate goversioninfo -manifest=chapi2client.exe.manifest
+
 // (c) Copyright 2019 Hewlett Packard Enterprise Development LP
 
 package main
